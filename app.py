6362b3b1ebc763bcbbe6f233048707bdfe429f7f
Giouxel Kasaika <uxell@Mac.fritz.box>
2025-06-25 14:07:11 +0200

    feature/ add_author, template add_author.html, return message after success

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from data_models import db
+from flask import Flask, render_template, request
+from data_models import db, Author, Book
 import os
+from datetime import datetime
+
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__)) #using abspath to work on from whenever you call the app.py
 db_path = os.path.join(basedir, 'data', 'library.sqlite') #using abspath to work on from whenever you call the app.py
@@ -18,6 +19,31 @@ db.init_app(app)
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/add_author', methods=['GET','POST'])
+def add_author():
+  if request.method == "GET":
+    return render_template("add_author.html")
+  else:
+    name = request.form.get('name')
+    birthdate = request.form.get('birthdate') #return string
+    date_of_death = request.form.get('date_of_death') #return string
+
+    # convert strings to dates
+    birth_date_obj = datetime.strptime(birthdate, "%Y-%m-%d").date()
+    death_date_obj = datetime.strptime(date_of_death, "%Y-%m-%d").date()
+
+    author = Author(
+            name = name,
+            birth_date = birth_date_obj,
+            date_of_death = death_date_obj
+        )
+
+    db.session.add(author)
+    db.session.commit()
+    return f"Author added: {name}, {birthdate}, {date_of_death}"
+
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
